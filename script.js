d8f382e1ae58d124626864557e9e92166732c181
BekzodTurobov <cosmos.4618282@gmail.com>
2023-03-18 17:44:13 +0500

    fixed not found message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const searchBtn = document.getElementById("search-btn");
 const mealList = document.getElementById("meal");
 const mealDetailsContent = document.querySelector(".meal-details-content");
@@ -179,24 +181,22 @@ function selectFoodCategory(e) {
 // RENDER FOOD LIST
 function renderList(data) {
   let html = "";
-
   if (data.meals) {
     data.meals.forEach((meal) => {
       html += ` 
-      <div class = "meal-item" data-id='${meal.idMeal}'>
-        <div class = "meal-img">
-          <img src = "${meal.strMealThumb}" alt = "food">
-        </div>
-        <div class = "meal-name">
-          <h3>${meal.strMeal}</h3>
-          <a href = "#" class = "recipe-btn">Get Recipe</a>
-        </div>
-
-      </div>`;
+                <div class = "meal-item" data-id = "${meal.idMeal}">
+                    <div class = "meal-img">
+                        <img src = "${meal.strMealThumb}" alt = "food">
+                    </div>
+                    <div class = "meal-name">
+                        <h3>${meal.strMeal}</h3>
+                        <a href = "#" class = "recipe-btn">Get Recipe</a>
+                    </div>
+                </div>`;
     });
     mealList.classList.remove("notFound");
   } else {
-    mealList.innerHTML = `Sorry we can\'t find the meal.`;
+    html = "Sorry, we didn't find any meal!";
     mealList.classList.add("notFound");
   }
 
